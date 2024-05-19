a9cb4c4566ba89fd4ee7e003c2bdf5c1c6a1e23c
divas18 <bhadani.divas@gmail.com>
2024-05-19 12:16:02 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from "./pages/Home/Home"
-import Cart from "./pages/Cart/Cart"
-import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/cart' element={<Cart />}></Route>
-        <Route path='/order' element={<PlaceOrder />}></Route>
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/order" element={<PlaceOrder />}></Route>
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy & Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get In Touch</h2>
+          <ul>
+            <li>+91-7277101828</li>
+            <li>bhadani.divas@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © - All Right Reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
